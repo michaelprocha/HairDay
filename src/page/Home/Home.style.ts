b8ad4090890ce766463ebd51c2809ead90daac94
Michael Rocha <michaelrocha9722@gmail.com>
2026-07-02 15:51:13 -0300

    feat(home): add gap spacing between layout sections

--- src/page/Home/Home.style.ts
+++ src/page/Home/Home.style.ts
@@ -2,7 +2,7 @@ import { tv } from "tailwind-variants";
 
 const homeStyles = tv({
   slots: {
-    page: "min-h-dvh bg-gray-800 flex p-3",
+    page: "min-h-dvh bg-gray-800 flex p-3 gap-3",
   },
 });
 
