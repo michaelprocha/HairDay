d67d5ec6096d791c5673581b96a2e9cb6ecb086c
Michael Rocha <michaelrocha9722@gmail.com>
2026-07-02 16:46:38 -0300

    feat(brand): add SVG logo and update favicon
    Replace JPG logo with SVG version, add it to the Home page header, and update the favicon reference.

--- src/page/Home/Home.style.ts
+++ src/page/Home/Home.style.ts
@@ -2,7 +2,8 @@ import { tv } from "tailwind-variants";
 
 const homeStyles = tv({
   slots: {
-    page: "min-h-dvh bg-gray-800 flex p-3 gap-3",
+    page: "min-h-dvh bg-gray-800 flex p-3 gap-3 relative",
+    logo: "bg-gray-600 px-5 py-3 absolute top-0 left-0 rounded-br-xl",
   },
 });
 
--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,10 +1,14 @@
 import { homeStyles } from "./Home.style";
 import { Aside, Main } from "../../components/layout";
+import logoSvg from "../../assets/icons/Logo.svg";
 
 export function Home() {
-  const { page } = homeStyles();
+  const { page, logo } = homeStyles();
   return (
     <div className={page()}>
+      <div className={logo()}>
+        <img src={logoSvg} />
+      </div>
       <Aside />
       <Main />
     </div>
